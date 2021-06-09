0f2c6a4c5da908dcd78283572462e2d106dd425e
“TaimoorSaeed” <taimoor.dev@gmail.com>
2021-06-09 11:49:23 +0500

    Add seach capability

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -6,19 +6,33 @@ import {
   Param,
   Patch,
   Post,
+  Query,
 } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 import { Task, TaskStatus } from './tasks.model';
 import { title } from 'process';
 import { CreateTaskDto } from './dto/create-task.dto';
+import { GetTaskFilterDto } from './dto/get-task.dto';
 
 @Controller('tasks')
 export class TasksController {
   constructor(private tasksService: TasksService) {}
 
+  // @Get()
+  // getAllTasks(): Task[] {
+  //   return this.tasksService.getAllTasks();
+  // }
+
   @Get()
-  getAllTasks(): Task[] {
-    return this.tasksService.getAllTasks();
+  getTasks(@Query() filterDto: GetTaskFilterDto): Task[] {
+    // if we have any filters defined, call service getTaskWithFilters
+    // otherwise just get all tasks
+
+    if (Object.keys(filterDto).length) {
+      return this.tasksService.getTaskWithFilters(filterDto);
+    } else {
+      return this.tasksService.getAllTasks();
+    }
   }
 
   @Get('/:id')
